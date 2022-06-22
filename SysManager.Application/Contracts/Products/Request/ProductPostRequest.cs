c81d15f98d038cc6fad09ddeb452776bc9dae150
Matheus de Assis <assis4002@gmail.com>
2022-06-21 21:20:24 -0300

    Api.Admin

---
+++ SysManager.Application/Contracts/Products/Request/ProductPostRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SysManager.Application.Contracts.Users.Request
+{
+	public class UserPostRequest
+	{
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
